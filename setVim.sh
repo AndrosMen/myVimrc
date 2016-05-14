0026b53523125f18b9fea132a28c7093de82b01b
Andros <andr0s@outlook.com>
2016-05-14 15:47:39 -0500

    Update setVim.sh

--- setVim.sh
+++ setVim.sh
@@ -1,5 +1,7 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 sudo apt-get install build-essential cmake python-dev python3-dev -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mv /.gitConfs/andros/.vimrc ~/
+mv $DIR/.vimrc ~/
 vim +PluginInstall +qall
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
