817d59497af00370cc650df2a0785da1f44a1f85
Andros <andr0s@outlook.com>
2016-05-14 14:04:05 -0500

    Update setHost.sh

--- setHost.sh
+++ setHost.sh
@@ -5,6 +5,8 @@
 # some arguments don't have a corresponding value to go with it such
 # as in the --default example).
 # note: if this is set to > 0 the /etc/hosts part is not recognized ( may be a bug )
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 while [[ $# > 1 ]]
 do
 key="$1"
@@ -24,15 +26,16 @@ shift # past argument or value
 done
 
 sudo apt-get update 
-sudo apt-get install dconf-cli -y
-if [ $ISVIRTUAL eq 1 ] then
+sudo apt-get install dconf-cli vim -y
+if [ $ISVIRTUAL -eq 1 ];then
 	sudo apt-get install virtualbox-guest-x11
 fi 
 
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ~/.gitConfs/gnomeSolarized
 ~/.gitConfs/gnomeSolarized/install.sh -s dark_alternative -p Default
-bash ~/.gitConfs/setVim.sh
+bash $DIR/setVim.sh
 
-if [ $INSTJARVIS -eq 1 ] then
+if [ $INSTJARVIS -eq 1 ];then
 	bash setJarvis.sh
 fi
+
