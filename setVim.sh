be2211b17b5487fdfaf615761e47d9ae93f03d4d
Andros <andr0s@outlook.com>
2016-05-14 02:12:19 -0500

    Update setVim.sh

--- setVim.sh
+++ setVim.sh
@@ -1,6 +1,5 @@
 sudo apt-get install build-essential cmake python-dev python3-dev -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/andros-mendoza/myVimrc.git /.gitConfs/andros
 mv /.gitConfs/andros/.vimrc ~/
 vim +PluginInstall +qall
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
