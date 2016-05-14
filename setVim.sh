be7c49769cbff8714c89c6587774bc5b436a3bb4
Andros <andr0s@outlook.com>
2016-05-14 02:01:36 -0500

    Update setVim.sh
    Removed git from install

--- setVim.sh
+++ setVim.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git build-essential cmake python-dev python3-dev -y
+sudo apt-get install build-essential cmake python-dev python3-dev -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/andros-mendoza/myVimrc.git /.gitConfs/andros
 mv /.gitConfs/andros/.vimrc ~/
